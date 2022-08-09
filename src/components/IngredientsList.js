16d444b3fda42a6d0346441dd91383293b79fabc
thomasreynolds4881 <treynolds3610@gmail.com>
2022-08-08 23:35:03 -0500

    Improved ingredients list

--- src/components/IngredientsList.js
+++ src/components/IngredientsList.js
@@ -1,14 +1,24 @@
 import React from "react";
 
 const IngredientsList = ({ measures, ingredients }) => {
-    // lol
+
+    // build the list of ingredients
+    let ingredients_measures = [];
+    let measure = "";
+    for (let i = 0; i < ingredients.length; i++) {
+        measures[i] ? measure = ' [ ' + measures[i] + ']' : measure = "";
+        if (ingredients[i])
+            ingredients_measures.push(ingredients[i] + measure);
+    }
+
+    // return render of the list of ingredients
     return (
         <div className="ingredients-list">
-            {ingredients.map((ingredient, index) => {
-                if (ingredient) {
+            {ingredients_measures.map((item, index) => {
+                if (item) {
                     return (
-                    <div key={index} className="ingredient">
-                        <span>{measures[index] + ingredient}</span>
+                    <div key={index} className="ingredient-item">
+                        <span>{item}</span>
                     </div>
                     );
                 } else {
