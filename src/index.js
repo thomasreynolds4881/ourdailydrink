8f7d7f5a3548a64daf0f00ed58d2824f1a5e7637
thomasreynolds4881 <treynolds3610@gmail.com>
2022-08-11 01:09:07 -0500

    Fixed day off issue

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import CocktailCard from './components/CocktailCard';
 import Footer from './components/Footer';
-import CountDownTimer from "./components/CountDownTimer";
+//import CountDownTimer from "./components/CountDownTimer";
 
 const { useState } = React;
-const currentTime = new Date();
-let tomorrow = currentTime.getTime() + 85400000; // i think this logic is wrong
+//const currentTime = new Date();
+//let tomorrow = currentTime.getTime() + 85400000; // i think this logic is wrong
 
 const App = () => {
 
@@ -17,7 +17,7 @@ const App = () => {
   // get the cocktail from the API endpoint
   const fetchCocktail = async () => {
     //console.log('fetching cocktail...');
-    const res = await fetch(`https://us-east-1.aws.data.mongodb-api.com/app/api-vuizz/endpoint/cocktail`);
+    const res = await fetch(`https://us-east-1.aws.data.mongodb-api.com/app/api-vuizz/endpoint/today`);
     const data = await res.json();
     //console.log('connection established');
     //console.log(data);
@@ -28,12 +28,12 @@ const App = () => {
   if (cocktail) {
     return (
       <div>
-        <h1 className='today'>Today's Cocktail...</h1>
+        <h1 className='today'>Today's Drink</h1>
         <hr/>
         <CocktailCard drink={cocktail} />
-        <div className="countdown-container">
+        {/*<div className="countdown-container">
           <h4 className="countdown-text">Next cocktail:</h4><CountDownTimer countDownTimeStamp={tomorrow}/>
-        </div>
+        </div>*/}
         <Footer />
       </div>
     );
